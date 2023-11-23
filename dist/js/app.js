8e801d894a7ad32779cfd2d6e6c8ea39de1f88aa
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-11-23 13:15:00 -0300

    Negociação, modelagem e regras

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,6 @@
-// alert('Olá!')
+import { negociacaoExterna } from "./models/negociacao.js";
+
+const negociacao = new negociacaoExterna(new Date(), 10, 100);
+ console.log(negociacao);
+ negociacao.quantidade = 1000;
+ console.log(negociacao);

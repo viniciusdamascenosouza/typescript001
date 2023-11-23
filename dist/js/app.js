223373937de5ba436df2f64b9d3ec40ab47ce57b
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-11-23 13:47:24 -0300

    Finalização do modelo

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,6 @@
 import { negociacaoExterna } from "./models/negociacao.js";
 
 const negociacao = new negociacaoExterna(new Date(), 10, 100);
- console.log(negociacao);
- negociacao.quantidade = 1000;
- console.log(negociacao);
+console.log(
+  `A negociação foi realizada ${negociacao.data}, quantidade: ${negociacao.quantidade}, cada uma com valor de ${negociacao.valor}, totalizando ${negociacao.volume}`
+);
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,11 +1,56 @@
-export class negociacaoExterna {
-  #data;
-  #quantidade;
-  #valor;
+// export class negociacaoExterna {
+//   #data;
+//   #quantidade;
+//   #valor;
 
-  constructor(data, quantidade, valor) {
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;
-  }
+//   constructor(data, quantidade, valor) {
+//     this.#data = data;
+//     this.#quantidade = quantidade;
+//     this.#valor = valor;
+//   }
+
+//   get data() {
+//     return this.#data;
+//   }
+
+//   get quantidade() {
+//     return this.#quantidade;
+//   }
+
+//   get valor() {
+//     return this.#valor;
+//   }
+
+//   get volume() {
+//     return this.#quantidade * this.#valor;
+//   }
+// }
+
+
+export class negociacaoExterna {
+    #data;
+    #quantidade;
+    #valor;
+
+    constructor(data, quantidade, valor) {
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+    }
+
+    get data() {
+        return this.#data;
+    }
+
+    get quantidade() {
+        return this.#quantidade;
+    }
+
+    get valor () {
+        return this.#valor;
+    }
+
+    get volume() {
+        return this.#quantidade * this.#valor;
+    }
 }

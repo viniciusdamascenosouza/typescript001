47e5b98e2ca02790f003649e8c0e7f1d634bf7a1
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-11-23 17:09:01 -0300

    O modificador private

--- app/app.ts
+++ app/app.ts
@@ -3,4 +3,3 @@ import { negociacaoExterna } from "./models/negociacao.js";
 const negociacao = new negociacaoExterna(new Date(), 10, 100);
 console.log(negociacao.volume);
 
-alert('oi')
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,3 @@
 import { negociacaoExterna } from "./models/negociacao.js";
 const negociacao = new negociacaoExterna(new Date(), 10, 100);
 console.log(negociacao.volume);
-alert('oi');

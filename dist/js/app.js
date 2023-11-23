07e7580953cf23f27ac45c1e91df2777c1d78946
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-11-23 14:11:15 -0300

    Motivação do TypeScript

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,4 @@
 import { negociacaoExterna } from "./models/negociacao.js";
 
-const negociacao = new negociacaoExterna(new Date(), 10, 100);
-console.log(
-  `A negociação foi realizada ${negociacao.data}, quantidade: ${negociacao.quantidade}, cada uma com valor de ${negociacao.valor}, totalizando ${negociacao.volume}`
-);
+const negociacao = new negociacaoExterna(new Date());
+console.log(negociacao.volume)
